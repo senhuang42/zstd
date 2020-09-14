796d08d3d4d4f18e775957c81774c9b071ab98b8
senhuang42 <senhuang96@fb.com>
2020-09-14 17:55:59 -0400

    Expand rawSeqStore_t to include absolute positions of sequences

--- lib/compress/zstd_compress_internal.h
+++ lib/compress/zstd_compress_internal.h
@@ -190,10 +190,11 @@ typedef struct {
 } rawSeq;
 
 typedef struct {
-  rawSeq* seq;     /* The start of the sequences */
-  size_t pos;      /* The position where reading stopped. <= size. */
-  size_t size;     /* The number of sequences. <= capacity. */
-  size_t capacity; /* The capacity starting from `seq` pointer */
+  rawSeq* seq;          /* The start of the sequences */
+  size_t* absPositions; /* The absolute position of each sequence within a bytestream */
+  size_t pos;           /* The position where reading stopped. <= size. */
+  size_t size;          /* The number of sequences. <= capacity. */
+  size_t capacity;      /* The capacity starting from `seq` pointer */
 } rawSeqStore_t;
 
 typedef struct {
