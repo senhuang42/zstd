d38cc988f1f78532314fb8df17089a18cfe7b248
Sen Huang <senhuang96@fb.com>
2021-03-01 13:49:51 -0800

    Add in workspace changes

--- programs/fileio.c
+++ programs/fileio.c
@@ -1323,8 +1323,6 @@ FIO_compressZstdFrame(FIO_ctx_t* const fCtx,
     }
     (void)srcFileName;
 
-    ZSTD_CCtx_setParameter(ress.cctx, ZSTD_c_useRowMatchfinder, 1);
-
     /* Main compression loop */
     do {
         size_t stillToFlush;
